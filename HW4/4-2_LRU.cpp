1139e63138d9de6c2ee7982114fba6416ea1e25b
Lily Wu <20021212lilywu@gmail.com>
2023-12-01 12:46:24 +0800

    12/1

--- HW4/4-2_LRU.cpp
+++ HW4/4-2_LRU.cpp
@@ -3,9 +3,45 @@
 using namespace std;
 
 int main(void){
-    int blocks, k, n; 
-    cin >> blocks, k, n;
+    int blocks, k, n, miss = 0; 
+    cin >> blocks >> k >> n;
     int sets = blocks/k; 
-    
+    vector<vector<int> > cache(sets); // cache -> sets -> blocks
+    for (int index=0; index<n; index++){
+        bool found = false; 
+        int data; 
+        cin >> data;
+        for(int i=0; i<sets; i++){
+            for (int j=0; j<cache[i].size(); j++){
+                // data is found 
+                if (data == cache[i][j]){ 
+                    // move data to the back (most recently used)
+                    // cout << index << " found: " << data << " at (" << i << ", " << j << ")\n";
+                    for(int l=cache[i].size()-2; l>=j; l--){ 
+                        cache[i][l] = cache[i][l+1];
+                    } cache[i][cache[i].size()-1] = data;
+                    found = true; 
+                    break; 
+                } 
+            }
+            if (found) break; 
+        }
+        if (!found){
+            int s = index % sets; 
+            // data is not found and set isn't full
+            if (cache[s].size() < k){
+                cache[s].push_back(data);
+            } 
+            // data is not found and set is full
+            else {
+                for (int j=0; j<k-1; j++){
+                    cache[s][j] = cache[s][j+1];
+                } cache[s][k-1] = data;
+            }
+            // cout << index << " pushed: " << data << " at (" << s << ", " << cache[s].size()-1 << ")\n";
+            miss++;
+        }
+    }
+    cout << "Total Cache Misses: " << miss << endl; 
     return 0;
 }
