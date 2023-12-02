af2fd0bf1f85f88cbe753682d36a1ad989942393
Lily Wu <20021212lilywu@gmail.com>
2023-12-02 21:10:08 +0800

    12/2

--- HW4/4-2_LFU.cpp
+++ HW4/4-2_LFU.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
 int main(void){
@@ -7,6 +8,7 @@ int main(void){
     cin >> blocks >> k >> n;
     int sets = blocks/k; 
     vector<vector<int> > cache(sets); // cache -> sets -> blocks
+    vector<vector<int> > freq(sets); // frequency of each data
     for (int index=0; index<n; index++){
         bool found = false; 
         int data; 
@@ -15,11 +17,15 @@ int main(void){
             for (int j=0; j<cache[i].size(); j++){
                 // data is found 
                 if (data == cache[i][j]){ 
+                    int f = freq[i][j]; 
                     // move data to the back (most recently used)
                     cout << "[ O ] " << data << " found at (" << i << ", " << j << ")\n";
                     for(int l=j; l<cache[i].size()-1; l++){ 
                         cache[i][l] = cache[i][l+1];
-                    } cache[i][cache[i].size()-1] = data;
+                        freq[i][l] = freq[i][l+1];
+                    }
+                    cache[i][cache[i].size()-1] = data;
+                    freq[i][cache[i].size()-1] = f+1; 
                     found = true; 
                     break; 
                 } 
@@ -31,12 +37,24 @@ int main(void){
             // data is not found and set isn't full
             if (cache[s].size() < k){
                 cache[s].push_back(data);
+                freq[s].push_back(0);
             } 
             // data is not found and set is full
             else {
-                for (int j=0; j<k-1; j++){
+                int least_freq = INT_MAX;
+                int freq_index = 0; 
+                for (int j=k-1; j>=0; j--){
+                    if (freq[s][j] < least_freq){
+                        least_freq = freq[s][j];
+                        freq_index = j; 
+                    }
+                }
+                for (int j=freq_index; j<k-1; j++){
                     cache[s][j] = cache[s][j+1];
-                } cache[s][k-1] = data;
+                    freq[s][j] = freq[s][j+1];
+                } 
+                cache[s][k-1] = data;
+                freq[s][k-1] = 0;
             }
             cout << "[ X ] " << data << " pushed at (" << s << ", " << cache[s].size()-1 << ")\n";
             miss++;
@@ -45,6 +63,10 @@ int main(void){
             cout << "cache " << i << ": ";
             for (int j=0; j<cache[i].size(); j++){
                 cout << cache[i][j] << " ";
+            } 
+            cout << "\tfreq " << i << ": "; 
+            for (int j=0; j<freq[i].size(); j++){
+                cout << freq[i][j] << " ";
             } cout << endl; 
         } cout << endl; 
     }
